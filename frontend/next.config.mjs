dda358404304d24c3226843cf6d77cc7473792f4
ngocdiip <luongngocdiep2017@gmail.com>
2026-06-03 10:19:12 +0700

    fix: downgrade @react-three/drei to version 9.105.6 for compatibility; update gallery images and descriptions to reflect historical context; add @react-three/fiber as a dependency in root package.json

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,14 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.vnecdn.net',
+      },
     ],
   },
 };
